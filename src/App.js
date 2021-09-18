8b6d8624296c2f8678818c17854cb568278c5fc5
Sam-kanchan <kanchansamuel26@gmail.com>
2021-09-18 11:35:52 +0530

    message

---
+++ src/App.js
@@ -0,0 +1,31 @@
+// //class based component
+import React from "react";
+import { Component } from "react";
+
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    return <h1 className="App">Hello React!!!</h1>
+  }
+}
+
+export default App;
+
+
+////Functional based component
+
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       Hello to React!
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
