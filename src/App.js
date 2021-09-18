3bb94ff4eef6b2b9755c70cdafdea6c466bba019
Sam-kanchan <kanchansamuel26@gmail.com>
2021-09-18 13:08:33 +0530

    dynamic name and hobbies

--- src/App.js
+++ src/App.js
@@ -9,12 +9,19 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
 
-        <Person />
+      <div className="App">
+        <Person name="Alex">
+          <p>I like cricket</p>
+        </Person>
+        <Person name="Bob">
+          <p>I like football</p>
+        </Person>
+        <Person name="Mary">
+          <p>I like tennis</p>
+        </Person>
+
+        <Person name="Mark" />
       </div>
     )
   }
--- src/Person.js
+++ src/Person.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 
-// function Person() {
-//     return <p>Hi I am a person - Functional</p>;
+// function Person(props) {
+//     return (
+//         <div>
+//             <div>
+//                 <p>Hi I am {props.name}, having age {Math.floor(Math.random() * 30)}</p>
+//             </div>
+//             <p>{props.children}</p>
+//         </div>
+//     );
 // }
-
+/////props: arguments
 //class based
 class Person extends Component {
     render() {
-        return <p>Hi I am a person having age {Math.floor(Math.random() * 30)}</p>
+        return (
+            < div >
+                <div>
+                    <p>Hi I am {this.props.name}, having age {Math.floor(Math.random() * 30)}</p>
+                </div>
+                <p>{this.props.children}</p>
+            </div >
+        );
     }
 }
 export default Person;
